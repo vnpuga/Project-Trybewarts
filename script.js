8b48ce5f851cc8a3a80eabdb5aa5b1b236e99536
Viviane Puga <vnpuga@gmail.com>
2022-01-17 07:14:44 -0300

    adicona comentarios no codigo

--- script.js
+++ script.js
@@ -15,21 +15,31 @@ function validate() {
 }
 validate();
 
+// REQUISITO 18 referências:
+// https://developer.mozilla.org/en-US/docs/Web/API/HTMLSelectElement/disabled
+// http://www.w3bai.com/pt/tags/att_checked.html
+// disable é um atributo booleano, e se ele estiver definido, os 'controles' serão desabilitados (nosso caso, o botão);
+// checked é um atributo booleano, e qdo usado, especifica q um elemento <input> deve ser pré-selecionado;
 const checkBox = document.getElementById('agreement');
 const submitBtn = document.getElementById('submit-btn');
 
-submitBtn.disabled = true;
+submitBtn.disabled = true; // botão desabilitado
 
 checkBox.addEventListener('click', () => {
-  if (checkBox.checked === false) {
+  if (checkBox.checked === false) { // checkbox não está pré-selecionado (checked = false).
     submitBtn.disabled = true;
   } else {
-    submitBtn.disabled = false;
+    submitBtn.disabled = false; // senão, o botão está habilitado.
   }
 });
 
-// REQUISITO 18 referência:https://developer.mozilla.org/en-US/docs/Web/API/HTMLSelectElement/disabled
-
+// Requisito 20 referências:
+// https://developer.mozilla.org/en-US/docs/Web/API/Document/keyup_event
+// https://developer.mozilla.org/en-US/docs/Web/Events
+// https://www.w3schools.com/jsref/prop_text_value.asp
+// o evento keyup é acinonado quando uma tecla é liberada (no caso abaixo, sempre q digitar no campo textarea).
+// a propriedade .value contém o valor do campor de texto.
+// a propriedade .innerText (DOM) especifica o conteúdo de texto de um nó.
 textArea.addEventListener('keyup', () => {
   counter.innerText = 500 - textArea.value.length;
 });
